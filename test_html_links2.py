cd0cb4831e644f42c824ef9da25a34c9eee586de
MOPH Developer <dev@anamai.moph.go.th>
2026-09-25 09:51:47 +0700

    Fix template download links and favicon to use BASE_URL

---
+++ test_html_links2.py
@@ -0,0 +1,21 @@
+﻿import os
+import glob
+
+files = glob.glob('templates/*.html')
+
+for file in files:
+    with open(file, 'r', encoding='utf-8') as f:
+        content = f.read()
+    
+    replacements = [
+        ('href="/api/download_template/', 'href="{{ BASE_URL }}/api/download_template/'),
+        ('href="/static/ICON_FLUORIDE.png"', 'href="{{ BASE_URL }}/static/ICON_FLUORIDE.png"')
+    ]
+    
+    for old, new in replacements:
+        content = content.replace(old, new)
+        
+    with open(file, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+print("Updated remaining hardcoded links in HTML files.")
